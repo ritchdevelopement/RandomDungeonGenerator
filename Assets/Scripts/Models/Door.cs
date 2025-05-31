2374b8db3393eb01d2bdafa5678829e30a21e7db
Rami El Kudr <raelkudr@gmail.com>
2025-05-31 15:48:37 +0200

    Calculate size of door based on tiles

--- Assets/Scripts/Models/Door.cs
+++ Assets/Scripts/Models/Door.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Door {
-    public List<Vector2Int> TilePositions { get; }
+    public List<Vector2Int> TilePositions { get; private set; }
+    public Vector2Int Size { get; private set; }
 
     public Door(List<Vector2Int> tilePositions) {
         if(tilePositions == null || tilePositions.Count == 0) {
@@ -10,5 +12,14 @@ public class Door {
         }
 
         TilePositions = tilePositions;
+        Size = CalculateSize();
+    }
+
+    private Vector2Int CalculateSize() {
+        int minX = TilePositions.Min(pos => pos.x);
+        int maxX = TilePositions.Max(pos => pos.x);
+        int minY = TilePositions.Min(pos => pos.y);
+        int maxY = TilePositions.Max(pos => pos.y);
+        return new Vector2Int(maxX - minX + 1, maxY - minY + 1);
     }
 }
